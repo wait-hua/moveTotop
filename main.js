cce4cbd86daa27fff7888887d70615791c92d356
jch <1424779709@qq.com>
2016-04-18 15:27:29 +0800

    add three files

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $(window).scroll( function() {               //滚动时触发
+        var top = $(document).scrollTop();       //获取滚动条到顶部的垂直高度
+        var height = $(window).height();     //获得可视浏览器的高度
+        if(top > 1/10*height){                           //到一定高度显示
+        
+            $("#moveTotop").fadeIn(300);
+        }else {                            //小于100消失
+            $("#moveTotop").fadeOut(300);
+        }
+    });
+    /*点击回到顶部*/
+    $('#moveTotop').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+});
